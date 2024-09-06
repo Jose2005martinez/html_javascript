2a07f2465e3d85f2e2e1c12c2161f095f71848f5
Jose Antonio Martinez Quintero <jose.martinez925@pascualbravo.edu.co>
2024-09-06 16:18:02 -0500

    ACTULIZACION CON VARIABLES LET Y CONST

---
+++ clase.js
@@ -0,0 +1,47 @@
+// uso de la variable let
+
+let numero1 = 2;
+let numero2 = 3;
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicasion = numero1 * numero2;
+let division  = numero1 / numero2;
+let esmayor = numero1>numero2;
+let promedio = numero1%numero2;
+let espar = numero1 %2 === 0;
+let nombre = "juan";
+
+
+// uso de variable conts
+
+const PI = 3.1416;
+const graverdad = 9.8;
+const velocidadluz = 299792458;
+const mensaje = "holaMundo";
+const Iva = 0.18;
+
+
+// opercion ternario 
+
+let resultado = numero1 > numero2 ? "numero 1 es mayor" : "numero 2 es mayor";
+let edad = 20;
+let mayorEdad = edad > 18  ? 'es mayor de edad' : 'no es mayor de edad';
+
+
+
+
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicasion);
+console.log(division);
+console.log(esmayor);
+console.log(nombre);
+console.log(espar);
+console.log(mayorEdad);
+console.log(resultado);
+
+console.log(PI);
+console.log(graverdad);
+console.log(mensaje)
+console.log(velocidadluz);
